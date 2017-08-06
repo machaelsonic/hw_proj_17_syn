310feaa3ed97c4c1f64cab6a4850f321c9f83087
hzw <hzw@hotmail.com>
2017-08-06 17:11:47 +0800

    burst方法无法满足设计要求.

--- fft_ip_tb.vhd
+++ fft_ip_tb.vhd
@@ -23,14 +23,8 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
--- use ieee.std_logic_unsigned.all;
--- use ieee.std_logic_arith.all;
--- use ieee.std_logic_signed.all;
-
 use ieee.numeric_std.all;
 use std.textio.all;
--- USE STD.TEXTIO.ALL;
--- USE IEEE.STD_LOGIC_TEXTIO.ALL; 
 entity fft_ip_tb is
 end fft_ip_tb;
 
@@ -71,7 +65,6 @@ architecture tb of fft_ip_tb is
   signal frames_out_index : natural range 0 to NUM_FRAMES_c := 0;
 
   signal cnt        : natural range 0 to 256;
-  signal cnt_t        : natural range 0 to 256;
   signal end_test   : std_logic;
   -- signal the end of the input data stream and output data stream.
   signal end_input  : std_logic;
@@ -104,24 +97,7 @@ architecture tb of fft_ip_tb is
       ); 
   end component fft_ip;
   
-  FILE tb_fft_real:TEXT OPEN WRITE_MODE IS "tb_fft_real.txt";
-  FILE tb_fft_imag:TEXT OPEN WRITE_MODE IS "tb_fft_imag.txt";
-  
 begin
-
-process(clk) is
- 	VARIABLE lo_1,lo_2:LINE;
-    BEGIN
-	if rising_edge(clk) then
-		if sink_valid='1' then
-		    WRITE (lo_1,to_integer(signed(sink_real)));
-			 WRITELINE (tb_fft_real,lo_1);
-			WRITE (lo_2,to_integer(signed(sink_imag)));
-			 WRITELINE (tb_fft_imag,lo_2); 
-			 
-		end if;
-	end if;
-end process;	
   -----------------------------------------------------------------------------------------------
   -- Reset Generation                                                                          
   -----------------------------------------------------------------------------------------------
@@ -178,23 +154,12 @@ end process;
     if reset_n = '0' then
       cnt <= 0;
     elsif rising_edge(clk) then
-      --if sink_valid = '1' and sink_ready = '1' then
-      if sink_ready = '1' then
+      if sink_valid = '1' and sink_ready = '1' then
         if cnt = fftpts_array(frames_in_index) - 1 then
           cnt <= 0;
         else
           cnt <= cnt + 1;
         end if;
-		
-		if cnt = 253 then
-			cnt_t <= 0;
-		elsif cnt = 254 then
-			cnt_t <= 1;
-		elsif cnt = 255 then
-			cnt_t <= 2;
-		else
-			cnt_t <= cnt + 3;
-		end if;
       end if;
     end if;
   end process cnt_p;
@@ -257,14 +222,6 @@ end process;
   end process end_test_p;
 
   -----------------------------------------------------------------------------------------------
-  -- Temp cnt 
-  -----------------------------------------------------------------------------------------------
-  -- process (cnt)
-  -- begin
-	-- cnt_t <= cnt - 1;
-  -- end process;
-  
-  -----------------------------------------------------------------------------------------------
   -- Read input data from files                                                                  
   -----------------------------------------------------------------------------------------------
   testbench_i : process(clk) is
@@ -290,10 +247,8 @@ end process;
           readline(i_file, idata);
           read(idata, data_i);
           sink_valid <= '1';
-          --sink_real  <= std_logic_vector(to_signed(data_r, 12));
-          --sink_imag  <= std_logic_vector(to_signed(data_i, 12));
-		  sink_real  <= std_logic_vector(to_signed(cnt_t, 12));
-		  sink_imag  <= std_logic_vector(to_signed(0, 12));
+          sink_real  <= std_logic_vector(to_signed(data_r, 12));
+          sink_imag  <= std_logic_vector(to_signed(data_i, 12));
         else
           sink_valid <= '1';
           sink_real  <= sink_real;
