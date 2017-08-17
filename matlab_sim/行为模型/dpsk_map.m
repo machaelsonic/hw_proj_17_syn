1b59e9917b5d6e6ce3ed9c044a5389cb38306e25
hzw <hzw_wizard@hotmail.com>
2017-08-17 18:08:30 +0800

    1.dpsk调制函数

---
+++ matlab_sim/行为模型/dpsk_map.m
@@ -0,0 +1,26 @@
+function [x,pre_phase_next]=dpsk_map(y,pre_phase)
+ [N,M]=size(y);
+%  M=33;%起始子载波编号
+%  C=36;%使用子载波数量
+  for k=1:M
+            if y(k)==0 
+                x_phase(k)=pre_phase(k);
+            else
+                if pre_phase(k)>=8
+                     x_phase(k)=pre_phase(k)-8;
+                else
+                     x_phase(k)=pre_phase(k)+8;
+                end 
+            end
+          x(k)=exp(i*x_phase(k)*pi/8);  
+   end
+        pre_phase_next=x_phase;
+        
+%         x=zeros(1,256);
+%         for k=M+1:1:M+C
+%             x(k)=exp(i*x_phase(k-M)*pi/8);
+%         end
+end
+        
+    
+
